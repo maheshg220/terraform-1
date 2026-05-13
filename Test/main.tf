f3b2ae094e6db112bd884f30c2694d18f22c0299
mahi <mahiasg214@gmail.com>
2026-05-13 22:03:05 +0530

    rg2 creation

--- Test/main.tf
+++ Test/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
   name = "mahesh-rg1"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name = "mahesh-rg2"
+  location = "centralindia"
+}
